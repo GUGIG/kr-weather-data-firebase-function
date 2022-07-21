6d4f64c4c49b1ed7c432b63674a4ad966a398cd7
정재훈 <jhc060109@naver.com>
2022-07-21 11:42:10 +0900

    replace city names w/ supported ones

--- functions/states.js
+++ functions/states.js
@@ -5,7 +5,8 @@ module.exports.states = [
   },
   {
     name: "Chungcheongbuk-do",
-    cities: ["Cheongju"],
+    // cities: ["Cheongju"],
+    cities: ["Yeongdong"],
   },
   {
     name: "Chungcheongnam-do",
@@ -21,7 +22,8 @@ module.exports.states = [
   },
   {
     name: "Gyeongsangnam-do",
-    cities: ["Jinju"],
+    // cities: ["Jinju"],
+    cities: ["Gimhae"],
   },
   {
     name: "Jeju-do",
@@ -29,11 +31,13 @@ module.exports.states = [
   },
   {
     name: "Jeollabuk-do",
-    cities: ["Jeonju"],
+    // cities: ["Jeonju"],
+    cities: ["Gunsan"],
   },
   {
     name: "Jeollanam-do",
-    cities: ["Suncheon"],
+    // cities: ["Suncheon"],
+    cities: ["Jangseong"],
   },
   {
     name: "Gwangju",
@@ -50,12 +54,8 @@ module.exports.states = [
 ];
 
 /** 
- * 한시간에 api call 13회(한국 내 13지역 데이터 불러오므로),
- * 하루 24시간이니깐 13회 * 24시간 >> 312회
+ * api call 13회 / hour (한국 내 13지역 데이터 불러오므로),
+ * 하루 호출: 24시간 13회 * 24시간 >> 312회
  * 한달 약 30일. => 312회 * 30일 >> 9,360회
- * 한달 무료 api call limit이 10,000회 이므로
- * 640회 여유 있음. 좀 간당간당한가?
- * api key 3개 돌려쓰도록 했다!,
- * => key1: 5회/h, key2: 5회/h, key3: 3회/h
- * => key1: 3,600회/month, key2: 3,600회/month, key3: 2,160회/month
+ * api call limit 10,000회
  */
